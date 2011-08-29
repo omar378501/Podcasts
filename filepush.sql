4c7538e186ee05ac5f6fdb3700dfccf559f68da1
Rodolfo D. Arce S <rodolfo.arce.s@gmail.com>
2011-08-29 09:49:37 -0400

    Agregado la tabla de archivos

--- filepush.sql
+++ filepush.sql
@@ -1,65 +1,129 @@
--- phpMyAdmin SQL Dump
--- version 3.4.3.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.1.58, for redhat-linux-gnu (i386)
 --
--- Servidor: localhost
--- Tiempo de generación: 26-08-2011 a las 15:20:45
--- Versión del servidor: 5.1.58
--- Versión de PHP: 5.3.6
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: filepush
+-- ------------------------------------------------------
+-- Server version	5.1.58
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `filepush`
+-- Table structure for table `course`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `course`
---
-
-CREATE TABLE IF NOT EXISTS `course` (
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique course ID',
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Course short name',
   `description` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Course description',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `user`
+-- Dumping data for table `course`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES (1,'INFO120','Introduccion a la informatica'),(2,'INFO121','Logica de programacion I');
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique file ID',
+  `filename` text NOT NULL COMMENT 'File name',
+  `date` date NOT NULL COMMENT 'File upload date',
+  `description` text NOT NULL,
+  `path` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='File table';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `file`
+--
+
+LOCK TABLES `file` WRITE;
+/*!40000 ALTER TABLE `file` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique user ID',
   `username` text NOT NULL COMMENT 'Username',
   `email` text NOT NULL COMMENT 'User email',
   `password` text NOT NULL COMMENT 'Encrypted password',
   `enabled` tinyint(4) NOT NULL COMMENT 'User is enabled',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='User data table' AUTO_INCREMENT=3 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='User data table';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `user_course`
+-- Dumping data for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user_course` (
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'rarce','rarce@uaa.edu.py','7c4a8d09ca3762af61e59520943dc26494f8941b',1),(2,'salcaraz','salcaraz@uaa.edu.py','7c4a8d09ca3762af61e59520943dc26494f8941b',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_course`
+--
+
+DROP TABLE IF EXISTS `user_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_course` (
   `user_id` int(11) NOT NULL COMMENT 'User ID',
   `course_id` int(11) NOT NULL COMMENT 'Course ID',
   `is_prof` tinyint(4) NOT NULL COMMENT 'User is professor'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `user_course`
+--
+
+LOCK TABLES `user_course` WRITE;
+/*!40000 ALTER TABLE `user_course` DISABLE KEYS */;
+INSERT INTO `user_course` VALUES (1,1,0),(2,2,0),(1,2,1),(2,1,1);
+/*!40000 ALTER TABLE `user_course` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-08-29  9:46:46
