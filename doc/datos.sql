7dadf6f3add6c4a291a2eef76127ba15f393e83e
Rodolfo D. Arce S. <rodolfo.arce.s@gmail.com>
2011-09-23 22:00:18 -0400

    Modificados ciertos indices y contenidos

--- doc/datos.sql
+++ doc/datos.sql
@@ -3,15 +3,15 @@ INSERT INTO course (id,name,description) VALUES (1,'INTRO-1','Introduccion a la 
 INSERT INTO course (id,name,description) VALUES (2,'JAVA-2','Java avanzado');
 # User
 INSERT INTO user (id,username,email,password,enabled) VALUES (1,'rarce','rarce@uaa.edu.py','7c4a8d09ca3762af61e59520943dc26494f8941b',1);
-INSERT INTO user (id,username,email,password,enabled) VALUES (1,'salcaraz','salcaraz@uaa.edu.py','7c4a8d09ca3762af61e59520943dc26494f8941b',1);
+INSERT INTO user (id,username,email,password,enabled) VALUES (2,'salcaraz','salcaraz@uaa.edu.py','7c4a8d09ca3762af61e59520943dc26494f8941b',1);
 # User-Course
 INSERT INTO user_course (user_id,course_id,is_prof) VALUES (1,1,1);
 INSERT INTO user_course (user_id,course_id,is_prof) VALUES (1,2,0);
 INSERT INTO user_course (user_id,course_id,is_prof) VALUES (2,2,1);
 INSERT INTO user_course (user_id,course_id,is_prof) VALUES (2,1,0);
 # File
-INSERT INTO file (id,filename,date,description,path) VALUES(1,'datos.sql','0000-00-00','files/1-a28db39a730d5c28764c0aaa64e8089ee757e6c9');
-INSERT INTO file (id,filename,date,description,path) VALUES(2,'filepush.sql','0000-00-00','files/2-42bd5bbffd8ebdbe39c735e0261e7055acdc9458');
+INSERT INTO file (id,filename,date,description,path) VALUES(1,'datos.sql','0000-00-00','','files/1-a28db39a730d5c28764c0aaa64e8089ee757e6c9');
+INSERT INTO file (id,filename,date,description,path) VALUES(2,'filepush.sql','0000-00-00','','files/2-42bd5bbffd8ebdbe39c735e0261e7055acdc9458');
 # User_File
 INSERT INTO user_file (user_id,file_id) VALUES (1,1);
 INSERT INTO user_file (user_id,file_id) VALUES (1,1);
