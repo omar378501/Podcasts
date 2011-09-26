5208b3492a7a052908feab23019a8777dd474382
Rodolfo D. Arce S. <rodolfo.arce.s@gmail.com>
2011-09-26 15:02:16 -0400

    Agregado generador de feeds con paso de parametros para muestra

---
+++ rss.php
@@ -0,0 +1,50 @@
+<?php
+/* Cargar archivo de configuracion */
+include ("./inc/config.php");
+
+$user_id = $_GET["user_id"];
+
+/* Establecer la conexion a la base de datos */
+$server = mysql_connect($db_server, $db_user, $db_pass); 
+if (!$server) die(mysql_error());
+mysql_select_db($db_name);
+
+/* Sentencia: mostrar los archivos de los cursos en los cuales el usuario
+* es participe, pero los archivos que el no es propietario */
+$query_file_list = sprintf("SELECT file.id,file.filename,file.description
+        FROM file,file_course,user_file,user_course
+        WHERE user_course.user_id = '%s' 
+        AND file_course.course_id = user_course.course_id
+        AND file.id = file_course.file_id
+        AND user_file.user_id = user_course.user_id
+        AND NOT file.id = user_file.user_id",
+        mysql_real_escape_string($user_id) );
+
+$result_file_list = mysql_query($query_file_list);
+
+/* Control de ejecucion */
+if (!$result_file_list) {
+        $message  = 'Sentencia invalida: ' . mysql_error() . "\n";
+        $message .= 'Whole query: ' . $query_file_list;
+        die($message);
+ } else {		
+        
+
+        echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+        echo '<feed xml:lang="es-PY" xmlns="http://www.w3.org/2005/Atom">' . "\n";
+        echo "\t<channel>\n";
+        echo "\t<title> Portal Multimedia </title>\n";
+        echo "\t<link href=\"http://www.uaa.edu.py\" />\n";
+                               
+        while ($row = mysql_fetch_assoc($result_file_list)) {
+                echo "\t\t<item>\n";
+                echo "\t\t\t<title>" . $row["filename"] . "</title>\n";
+                echo "\t\t\t<summary>" . $row["description"] . "</summary>\n";
+                echo "\t\t\t<link href=\"./download.php?id=" . $row["id"] . "\" />\n";
+                echo "\t\t</item>\n";              
+        }
+        echo "\t</channel>\n";
+        echo "</feed>\n";
+}
+
+?>
