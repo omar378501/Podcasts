a7544703e2b807a23726014e003cdca1379a4cc8
Rodolfo D. Arce S. <rodolfo.arce.s@gmail.com>
2011-09-26 13:39:13 -0400

    Creada la vista de "todos" en los cursos donde solo se ven los archivos correspondientes a mis cursos pero los cuales no soy propietario

--- course.php
+++ course.php
@@ -18,29 +18,44 @@ if ($_SESSION["access"] == "granted") {
 
                 /* Sentencia: buscar en la base de datos los cursos en los cuales participa,
                  * y si es profesor, el usuario en cuestion */
-                $query_file_list = sprintf("SELECT file.id,file.filename,file.description
+                if (isset($id)){
+                        $query_file_list = sprintf("SELECT file.id,file.filename,file.description
                         FROM file,file_course
-                        WHERE file_course.course_id='%s' AND file.id = file_course.file_id 
+                        WHERE file_course.course_id='%s' AND file.id = file_course.file_id
                         ORDER BY course_id",
                         mysql_real_escape_string($id));
+                        
+                } else {
+                        $query_file_list = sprintf("SELECT file.id,file.filename,file.description
+                        FROM file,file_course,user_file,user_course
+                        WHERE user_course.user_id = '%s' 
+                        AND file_course.course_id = user_course.course_id
+                        AND file.id = file_course.file_id
+                        AND user_file.user_id = user_course.user_id
+                        AND NOT file.id = user_file.user_id",
+                        mysql_real_escape_string($_SESSION["id"]) );
+                }
                 /* Hacer la consulta */
                 $result_file_list = mysql_query($query_file_list);
 
                 /* Control de ejecucion */
-                if (!$result_file_list) {
-                        $message  = 'Sentencia invalida: ' . mysql_error() . "\n";
-                        $message .= 'Whole query: ' . $query_file_list;
-                        die($message);
-                } else {		
+                        if (!$result_file_list) {
+                            $message  = 'Sentencia invalida: ' . mysql_error() . "\n";
+                             $message .= 'Whole query: ' . $query_file_list;
+                             die($message);
+                        } else {		
+                               echo "<b>Archivos del curso:</b><br>\n";
+                               
+                               while ($row = mysql_fetch_assoc($result_file_list)) {
 
-                        while ($row = mysql_fetch_assoc($result_file_list)) {
-                                echo "<b>Archivos del curso:</b><br>\n";
-                                echo $row["id"] . ": " .  $row["filename"] . "<br>\n";
+                                      echo $row["id"] . ": " .  $row["filename"] . "<br>\n";
+                                   }
                         }
+                
+                        
+                } else {
 
-        } else {
-
-                print ("Usuario deshabilitado");
+                        print ("Usuario deshabilitado");
 
         }
 
