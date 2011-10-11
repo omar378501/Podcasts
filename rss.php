69849749eb942a960cf10bac4585c8d76c74efe3
Rodolfo D. Arce S <rodolfo.arce.s@gmail.com>
2011-10-11 18:07:56 -0300

    Una nueva consulta para saber que cursos estan asociados a este archivo

--- rss.php
+++ rss.php
@@ -12,19 +12,31 @@ mysql_select_db($db_name);
 /* Sentencia: mostrar los archivos de los cursos en los cuales el usuario
 * es participe, pero los archivos que el no es propietario */
 $query_file_list = sprintf("SELECT DISTINCT file.id,file.filename,file.description
-                        FROM file,file_course,user_file,user_course
+                        FROM file,file_course,user_file,user_course,course
                         WHERE user_course.user_id = '%s' 
                         AND file_course.course_id = user_course.course_id
                         AND file.id = file_course.file_id
                         AND NOT user_file.user_id = user_course.user_id 
                         AND file.id = user_file.file_id
                         ORDER BY file.id DESC LIMIT 10",
-        mysql_real_escape_string($user_id) );
+                        mysql_real_escape_string($user_id) );
+
+$query_course_list = sprintf("SELECT DISTINCT file.id,file.filename,file.description
+                        FROM file,file_course,user_file,user_course,course
+                        WHERE user_course.user_id = '%s' 
+                        AND file_course.course_id = user_course.course_id
+                        AND file.id = file_course.file_id
+                        AND NOT user_file.user_id = user_course.user_id 
+                        AND file.id = user_file.file_id
+                        ORDER BY file.id DESC LIMIT 10",
+                        mysql_real_escape_string($user_id) ); 
 
 $result_file_list = mysql_query($query_file_list);
 
+$result_course_list = mysql_query($query_course_list);
+
 /* Control de ejecucion */
-if (!$result_file_list) {
+if (!$result_file_list OR !$result_course_list) {
         $message  = 'Sentencia invalida: ' . mysql_error() . "\n";
         $message .= 'Whole query: ' . $query_file_list;
         die($message);
@@ -41,12 +53,14 @@ if (!$result_file_list) {
         while ($row = mysql_fetch_assoc($result_file_list)) {
                 echo "\t\t<item>\n";
                 echo "\t\t\t<title>" . $row["filename"] . "</title>\n";
-                echo "\t\t\t<summary>" . $row["description"] . "</summary>\n";
+                echo "\t\t\t<summary>" . $row["course_id"]. $row["name"] . $row["description"] . "</summary>\n";
                 echo "\t\t\t<link href=\"./download.php?id=" . $row["id"] . "\" />\n";
                 echo "\t\t</item>\n";              
         }
         echo "\t</channel>\n";
         echo "</feed>\n";
+         
+         
 }
 
 ?>
